b470a8b6d72aa727f0364c1a38e3d2620c361f1d
andres <ag@todoo.co>
2020-02-21 23:23:02 +0000

    add uno

---
+++ vehiculos/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vehiculos",
+
+    'summary': """
+        modulo vehiculos andres gutierrez""",
+
+    'description': """
+        modulos de control de vehiculos
+    """,
+
+    'author': "Todoo",
+    'website': "http://www.todoo.co",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Vehiculos',
+    'version': '1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'views/views.xml',
+        #'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo/demo.xml',
+    ],
+}
---
+++ vehiculos/models/.ipynb_checkpoints/propietarios-checkpoint.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+ from odoo import models, fields
+
+ class propietarios(models.Model):
+     _name = 'propietarios.usuarios'
+     _description = 'propietarios de los vehiculos'
+
+     nombre propietario = fields.Char()
+     cedula = fields.Integer()
+     vehiculo = fields.Float(compute="_value_pc", store=True)
+     description = fields.Text()
+
+     @api.depends('value')
+     def _value_pc(self):
+         for record in self:
+             record.value2 = float(record.value) / 100
